7df4da06aaca68a757082cfef29b69bdece2ee13
Raí Santos da Soledade <raisoledade@outlook.com>
2017-08-22 12:00:33 -0400

    Update weasel.py

--- weasel.py
+++ weasel.py
@@ -2,17 +2,21 @@ import random
 import string
 
 padrao = "RAI SANTOS DA SOLEDADE"
+string_aleatoria = ""
 lista = []
 pontuacao = []
-probabilidade = 25
+probabilidade = 5
 maior_pontuacao = 0
 
-for i in range(10):
-    lista.append(padrao)
+for i in range(len(padrao)):
+    string_aleatoria = string_aleatoria+random.choice(string.ascii_uppercase+' ')
+
+for i in range(100):
+    lista.append(string_aleatoria)
     pontuacao.append(0)
 
 while(maior_pontuacao != len(padrao)):
-    for i in range(10):
+    for i in range(100):
         copia = lista[i]
         pontos = 0
         for j in range(len(padrao)):
@@ -28,5 +32,11 @@ while(maior_pontuacao != len(padrao)):
         pontuacao[i] = pontos
         #print(copia+' '+padrao+' ')
     maior_pontuacao = max(pontuacao)
+    if(maior_pontuacao != len(padrao)):
+    	melhor_string = lista[pontuacao.index(maior_pontuacao)]
+    	for i in range(100):
+    		lista[i]=melhor_string
+    		pontuacao[i]=maior_pontuacao
+
     print("maior-score-atual: "+str(maior_pontuacao)+" "+lista[pontuacao.index(maior_pontuacao)])
 #print(lista[pontuacao.index(maior_pontuacao)]+" score: "+str(maior_pontuacao))
